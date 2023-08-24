a7de10cc19725a85582896a47d3de0f520910c13
jsanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2023-08-24 00:24:21 -0300

    Ejercicio completo

---
+++ Assets/CoheteScritpt.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoheteScritpt : MonoBehaviour
+{
+    public float velocidadFinal = 0.1f;
+    public float velocidadActual;
+    public GameObject fuegoMotores;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fuegoMotores.SetActive(false);
+        Invoke("LanzarCohete",2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, velocidadActual, 0);
+        AnimacionFuegoMotores();
+    }
+
+    void LanzarCohete()
+    {
+        velocidadActual = velocidadFinal;
+        fuegoMotores.SetActive(true);
+    }
+
+    void AnimacionFuegoMotores()
+    {
+        fuegoMotores.transform.localScale = Vector3.one * Random.Range(0.5f,1.5f);
+    }
+}
---
+++ Assets/botonScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class botonScript : MonoBehaviour
+{
+    public GameObject botonNormal;
+    public GameObject botonPresionado;
+    public GameObject coheteFalso;
+    public GameObject coheteVerdadero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        botonNormal.SetActive(true);
+        botonPresionado.SetActive(false);
+        coheteFalso.SetActive(true);
+        coheteVerdadero.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        botonNormal.SetActive(false);
+        botonPresionado.SetActive(true);
+        coheteFalso.SetActive(false);
+        coheteVerdadero.SetActive(true);
+    }
+    void OnMouseUp()
+    {
+        botonNormal.SetActive(true);
+        botonPresionado.SetActive(false);
+    }
+}
